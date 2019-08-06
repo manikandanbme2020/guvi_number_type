fcb9eafa59b76a7335e0862218704e258972d6da
manikandanbme2020 <53809231+manikandanbme2020@users.noreply.github.com>
2019-08-06 11:51:22 +0530

    Update number_type.py

--- number_type.py
+++ number_type.py
@@ -1,8 +1,8 @@
 number = int(input())
 
-if number > 0:
-    print("Positive")
-elif number == 0:
+if number == 0:
     print("Zero")
+elif number > 0:
+    print("Positive")
 else:
     print("Negative")
